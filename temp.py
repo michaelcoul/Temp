5e14d259edbc946639eaa38cd2574bc526bbccf8
michaelcoul <michaelcoul63@gmail.com>
2023-12-17 20:07:08 +0400

    2

--- temp.py
+++ temp.py
@@ -1,30 +1,17 @@
 from flask import Flask, render_template
-import matplotlib.pyplot as plt
-import io
-import base64
-import random
+import requests
 
 app = Flask(__name__)
 
+def get_weather():
+    response = requests.get('http://wttr.in/Tolyatti?format=%C+%t')
+    return response.text
+
 @app.route('/')
 def home():
-    # Генерация случайных данных для температуры
-    # В реальном сценарии вы будете получать эти данные от Arduino и SIM800L
-    temperature_data = [random.randint(20, 40) for _ in range(10)]
-
-    # Создание графика
-    plt.figure(figsize=(10,5))
-    plt.plot(temperature_data)
-    plt.title('Temperature Data')
-    plt.grid(True)
-    plt.ylabel('Temperature (°C)')
-    plt.xlabel('Time')
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return render_template('home.html', plot_url=plot_url)
+    weather = get_weather()
+    return render_template('home.html', weather=weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
